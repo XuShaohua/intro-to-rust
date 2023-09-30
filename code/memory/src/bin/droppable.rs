d0085b3b90ec009be76e51037707b43a2e0f2158
Xu Shaohua <shaohua@biofan.org>
2023-10-01 00:02:58 +0800

    memory: Add droppable

---
+++ code/memory/src/bin/droppable.rs
@@ -0,0 +1,24 @@
+
+struct Droppable {
+  name: &'static str,
+}
+
+impl Drop for Droppable {
+  fn drop(&mut self) {
+    println!("> Dropping {}", self.name);
+  }
+}
+
+fn main() {
+  let _a = Droppable { name: "a" };
+  {
+    let _b = Droppable { name: "b" };
+    {
+      let _c = Droppable { name: "c"};
+      let _d = Droppable { name: "d"};
+    }
+  }
+  
+  println!("Dropping a manually");
+  drop(_a);
+}
