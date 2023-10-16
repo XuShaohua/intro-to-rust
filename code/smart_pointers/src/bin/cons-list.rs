a9f398ab184be6167c47be8ce85f3d30489857f9
Xu Shaohua <shaohua@biofan.org>
2023-10-17 00:00:29 +0800

    pointers: Add cons list

---
+++ code/smart_pointers/src/bin/cons-list.rs
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("list: {:?}", list);
+}
