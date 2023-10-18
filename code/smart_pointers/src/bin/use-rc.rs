6785c62b4d5c21f9071d5f9ae4b62e38163acd9f
Xu Shaohua <shaohua@biofan.org>
2023-10-19 00:01:20 +0800

    pointers: Add use-rc

---
+++ code/smart_pointers/src/bin/use-rc.rs
@@ -0,0 +1,30 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use std::rc::Rc;
+
+fn use_rc() {
+    let s = Rc::new("Hello".to_owned());
+    let t = s.clone();
+    let u = s.clone();
+
+    assert_eq!(s, t);
+    assert_eq!(s, u);
+    assert_eq!(Rc::strong_count(&s), 3);
+}
+
+fn test_weak_rc() {
+    let s = Rc::new("Hello".to_owned());
+    let t = Rc::clone(&s);
+    let u = Rc::clone(&s);
+
+    assert_eq!(s, t);
+    assert_eq!(s, u);
+    assert_eq!(Rc::strong_count(&s), 3);
+}
+
+fn main() {
+    use_rc();
+    test_weak_rc();
+}
