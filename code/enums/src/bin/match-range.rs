9f64499330faa2bdb7a2d84c0537e07c2be3a115
Xu Shaohua <shaohua@biofan.org>
2024-04-03 12:42:57 +0800

    enums: Add match range

---
+++ code/enums/src/bin/match-range.rs
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+fn main() {
+    let number = 13;
+
+    match number {
+        1 => println!("One!"),
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        13..=19 => println!(" A teen"),
+        _ => println!("Ain't special"),
+    }
+}
