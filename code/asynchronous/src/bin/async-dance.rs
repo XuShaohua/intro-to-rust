68db6390c003b399950fb15ca83998f4e885081a
Xu Shaohua <shaohua@biofan.org>
2024-04-03 09:11:26 +0800

    code: Add asynchronous

--- code/asynchronous/src/bin/async-dance.rs
+++ code/asynchronous/src/bin/async-dance.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be
+// found in the LICENSE file.
+
 use futures::executor::block_on;
 
 #[derive(Debug)]
--- code/traits/src/lib.rs
+++ code/traits/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (c) $YEAR Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by General Public License that can be
 // found in the LICENSE file.
 
