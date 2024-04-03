6db84942136418d79fc27cde864ca6e13b40196f
Xu Shaohua <shaohua@biofan.org>
2024-04-03 09:19:59 +0800

    tools: Add clippy script

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-# Use of this source is governed by General Public License that can be
-# found in the LICENSE file.
+# Use of this source is governed by General Public License that can be found
+# in the LICENSE file.
 
 set -xe
 
-cargo clippy
+cargo clippy --all-targets --all-features
 
 mdbook build
 #mdbook test
