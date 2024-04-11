1b2df1bab696228b1283799a3b4462edc5863230
Xu Shaohua <shaohua@biofan.org>
2024-04-11 15:45:52 +0800

    memory: Update swap()

--- code/memory/src/bin/mem-swap-take.rs
+++ code/memory/src/bin/mem-swap-take.rs
@@ -4,29 +4,41 @@
 
 use std::mem;
 
-#[allow(clippy::useless_vec)]
 fn mem_replace() {
-    let mut v = vec![Box::new(2), Box::new(3), Box::new(4)];
+    let mut v = [Box::new(2), Box::new(3), Box::new(4)];
 
     let ret = mem::replace(&mut v[1], Box::new(42));
     assert_eq!(*ret, 3);
     assert_eq!(*v[1], 42);
+
+    let mut v1 = vec![1, 2, 3];
+    let mut v2 = vec![4, 5];
+    (v1, v2) = (v2, v1);
+    v2 = mem::replace(&mut v1, v2);
+    assert_eq!(v2, vec![4, 5]);
 }
 
 fn mem_swap() {
-    let mut x = 42;
-    let mut y = 5;
+    let mut x = [1, 2, 3];
+    let mut y = [4, 5];
 
-    mem::swap(&mut x, &mut y);
-    assert_eq!(x, 5);
-    assert_eq!(y, 42);
+    mem::swap(&mut x[0], &mut y[0]);
+    assert_eq!(x, [4, 2, 3]);
+    assert_eq!(y, [1, 5]);
+}
+
+#[derive(Debug, Default)]
+struct Point {
+    x: f32,
+    y: f32,
 }
 
 fn mem_take() {
-    let mut v = vec![42, 43];
-    let old_v = mem::take(&mut v);
-    assert!(v.is_empty());
-    assert_eq!(old_v, [42, 43]);
+    let mut p = Point { x: 3.0, y: 4.0 };
+    let old_p = mem::take(&mut p);
+    assert_eq!(old_p.x, 3.0);
+    assert_eq!(p.x, 0.0);
+    assert_eq!(p.y, 0.0);
 }
 
 fn main() {
--- src/mem/mem/swap-fn.md
+++ src/mem/mem/swap-fn.md
@@ -1,1 +1,17 @@
 # mem::swap() 函数
+
+用于交换相同类型的两个可变引用.
+这个函数的内部使用了 [ptr::swap_nonoverlapping()](../ptr/swap-nonoverlapping-fn.md).
+
+```rust
+use std::mem;
+
+fn main() {
+    let mut x = [1, 2, 3];
+    let mut y = [4, 5];
+
+    mem::swap(&mut x[0], &mut y[0]);
+    assert_eq!(x, [4, 2, 3]);
+    assert_eq!(y, [1, 5]);
+}
+```
