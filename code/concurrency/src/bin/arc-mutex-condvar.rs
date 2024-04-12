fd3fbf0ded6fd0f83c7cf5a6cb5c21b569b8ef71
Xu Shaohua <shaohua@biofan.org>
2024-04-12 10:31:03 +0800

    concurrency: Add arc mutex condvar

---
+++ code/concurrency/src/bin/arc-mutex-condvar.rs
@@ -0,0 +1,24 @@
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
+
+fn main() {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    thread::spawn(move || {
+        let (lock, cvar) = &*pair2;
+        let mut started = lock.lock().unwrap();
+        *started = true;
+        cvar.notify_one();
+    });
+    let (lock, cvar) = &*pair;
+    let mut started = lock.lock().unwrap();
+    while !(*started) {
+        started = cvar.wait(started).unwrap();
+    }
+    println!("value changed");
+}
