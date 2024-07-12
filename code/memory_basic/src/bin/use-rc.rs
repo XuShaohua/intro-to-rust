18e77b59ea5357759b593d1fbc64f0d2233119bc
Xu Shaohua <shaohua@biofan.org>
2024-07-12 15:00:14 +0800

    memory-basic: Move ownership to its own chapter

---
+++ code/memory_basic/src/bin/use-rc.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use std::mem::size_of;
+use std::rc::Rc;
+
+fn main() {
+    assert_eq!(size_of::<Rc<i32>>(), 8);
+}
