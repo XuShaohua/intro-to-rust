8b349182424bdfc72d42dc4b47040a81519a8ded
Xu Shaohua <shaohua@biofan.org>
2024-07-23 18:19:25 +0800

    smart-pointers: Const slice to void*

--- code/smart_pointers/src/bin/c-void-pointer.rs
+++ code/smart_pointers/src/bin/c-void-pointer.rs
@@ -9,5 +9,22 @@ fn main() {
         *x_mut_ref = 43;
     }
     assert_eq!(x, 43);
+
+    // void**
+    unsafe {
+        let x_ref_ref = &x_addr as *const usize as *const *mut i32;
+        (*(*x_ref_ref)) = 44;
+    }
+    assert_eq!(x, 44);
+
+    // [u8; 4] to *const u8 to *const ()
+    {
+        let nums = [0, 1, 2, 3];
+        let nums_ptr: *const u8 = nums.as_ptr();
+        let nums_void_ptr: *const () = nums_ptr as *const ();
+        let nums_addr: usize = nums_void_ptr as usize;
+        assert_eq!(nums_addr, nums.as_ptr() as usize);
+    }
+
     println!("x: {x}");
 }
